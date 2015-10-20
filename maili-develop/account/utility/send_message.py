ffb9064d2f65e9f071a1bd156be0c22bb4459977
fortymiles <mqgao@outlook.com>
2015-10-20 11:31:31 +0800

    create new dir maili-develop

---
+++ maili-develop/account/utility/send_message.py
@@ -0,0 +1,13 @@
+"""
+Send message by a phone number.
+"""
+
+
+def send_message(phone):
+    '''
+    Sends message by phone number
+    ---
+    args:
+        phone: phone number
+    '''
+    print("sending message..")
