27c07802a1be678a89fefa04681b0c9a9074333b
fortymiles <mqgao@outlook.com>
2015-10-20 11:29:27 +0800

    move minquan's code inot maili-develop temperoraily

--- account/serializers.py
+++
@@ -1,8 +0,0 @@
-from account.models import User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('phone', 'password', 'last_name')
--- account/utility/generate_verification_code.py
+++
@@ -1,5 +0,0 @@
-def get_vericication_code(phone):
-    '''
-    Returns vericition code from a phone numbe.
-    '''
-    return phone[-6:]
