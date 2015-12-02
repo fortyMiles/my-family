3566c7689f59715f2d58886f52d3dd0c00a0ce4e
fortyMiles <mqgao@outlook.com>
2015-12-02 14:51:54 +0800

    change manage.py

--- maili-develop/manage.py
+++ maili-develop/manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maili.settings")
+    if 'test' in sys.argv:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maili.settings")
 
     from django.core.management import execute_from_command_line
 
