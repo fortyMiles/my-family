0fdf2a8a64602769d61cb787e088e32882be6aed
fortyMiles <mqgao@outlook.com>
2015-12-03 16:21:37 +0800

    update manage.py

--- maili-develop/manage.py
+++ maili-develop/manage.py
@@ -3,10 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
+    setting_url = 'maili.settings'
     if 'test' in sys.argv:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test.settings")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maili.settings")
+        setting_url =  "test.settings"
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting_url)
 
     from django.core.management import execute_from_command_line
 
